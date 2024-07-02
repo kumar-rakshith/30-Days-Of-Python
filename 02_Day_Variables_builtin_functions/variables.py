1f8f1410138e63c1cead49f8f1aa2ad5e8f7301e
Rakshith Kumar <rakshithkumar.thokur@gmail.com>
2024-07-02 20:58:46 +0530

    Update variables.py

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,6 +1,6 @@
-
 # Variables in Python
 
+# Single variable assignments
 first_name = 'Asabeneh'
 last_name = 'Yetayeh'
 country = 'Finland'
@@ -13,10 +13,9 @@ person_info = {
     'lastname':'Yetayeh', 
     'country':'Finland',
     'city':'Helsinki'
-    }
+}
 
 # Printing the values stored in the variables
-
 print('First name:', first_name)
 print('First name length:', len(first_name))
 print('Last name: ', last_name)
@@ -29,8 +28,7 @@ print('Skills: ', skills)
 print('Person information: ', person_info)
 
 # Declaring multiple variables in one line
-
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsinki', 250, True
 
 print(first_name, last_name, country, age, is_married)
 print('First name:', first_name)
@@ -38,3 +36,18 @@ print('Last name: ', last_name)
 print('Country: ', country)
 print('Age: ', age)
 print('Married: ', is_married)
+
+# Additional examples
+website = 'asabeneh.com'
+job = 'teacher'
+salary = 2500.00
+print('Website:', website)
+print('Job:', job)
+print('Salary:', salary)
+
+# Swapping variables
+a = 5
+b = 10
+print('Before swapping: a =', a, 'and b =', b)
+a, b = b, a
+print('After swapping: a =', a, 'and b =', b)
